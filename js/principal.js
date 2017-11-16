00bf46e2eb4c5ec7dfa60b5b8e20a798e0b07aec
William Amorim <william.amorim@tedbrasil.com>
2017-11-16 21:55:52 -0200

    incluindo formulario e colocando o evento no botão para não resetar a pagina

--- js/principal.js
+++ js/principal.js
@@ -33,4 +33,10 @@ for (var i = 0; i < pacientes.length; i++) {
         var imc = peso / (altura * altura);
         tdImc.textContent = imc.toFixed(2);
     }
+
+    var botaoAdicionar = document.querySelector('#adicionar-paciente');
+    botaoAdicionar.addEventListener("click", function(event){
+        event.preventDefault();
+        console.log("fui clicado");
+    })
 }
