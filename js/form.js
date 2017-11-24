1bba2b88d7ab12726b22eb10703dae573e374ec0
William Amorim <william.amorim@tedbrasil.com>
2017-11-24 16:28:09 -0200

    criando a função obtemPacienteDoFormulario para melhorar o código

--- js/form.js
+++ js/form.js
@@ -1,45 +1,55 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
-event.preventDefault();
+    event.preventDefault();
 
+    //seleciona o form
+    var form = document.querySelector("#form-adiciona");
 
-//seleciona o form
-var form = document.querySelector("#form-adiciona");
+    //chama a função que pega os dados do paciente
+    var paciente = obtemPacienteDoFormulario(form);
+    console.log(paciente);
 
-//acesso aos inputs do form e pegando o valor digitado com o VALUE
-var nome = form.nome.value;
-var peso = form.peso.value;
-var altura = form.altura.value;
-var gordura = form.gordura.value;
+    //função que pega os dados do paciente atravez de um objeto(var paciente)
+    function obtemPacienteDoFormulario(form) {
 
-//cria uma tr no document com o createElement
-var pacienteTr = document.createElement("tr");
+        var paciente = {
+            nome: form.nome.value,
+            peso: form.peso.value,
+            altura: form.altura.value,
+            gordura: form.gordura.value,
+            imc: calculaImc(form.altura.value, form.peso.value) //chama a função calculaImc e passa os valores dentro dela
+        }
 
-//cria os td's que ficarão dentro da tr
-var nomeTd = document.createElement("td");
-var pesoTd = document.createElement("td");
-var alturaTd = document.createElement("td");
-var gorduraTd = document.createElement("td");
-var imcTd = document.createElement("td");
+        return paciente
+    }
 
-//colocando os valores digitados no form para o td
-nomeTd.textContent = nome;
-pesoTd.textContent = peso;
-alturaTd.textContent = altura;
-gorduraTd.textContent = gordura;
-imcTd.textContent = calculaImc(peso, altura);
+    //cria uma tr no document com o createElement
+    var pacienteTr = document.createElement("tr");
 
-//colocando os td's dentro da tr
-pacienteTr.appendChild(nomeTd);
-pacienteTr.appendChild(pesoTd);
-pacienteTr.appendChild(alturaTd);
-pacienteTr.appendChild(gorduraTd);
-pacienteTr.appendChild(imcTd);
+    //cria os td's que ficarão dentro da tr
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
 
+    //colocando os valores digitados no form para o td
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
 
-//selecionando a tabela pelo id
-var tabela = document.querySelector("#tabela-pacientes");
+    //colocando os td's dentro da tr
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
-//inserindo o tr na tabela com o appendChild
-tabela.appendChild(pacienteTr);
+    //selecionando a tabela pelo id
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    //inserindo o tr na tabela com o appendChild
+    tabela.appendChild(pacienteTr);
 });
