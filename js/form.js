a9460994bb9152801c44a25dc82445a874b5f6cb
William Amorim <william.amorim@tedbrasil.com>
2017-12-07 16:48:43 -0200

    adicionado as validações dos campos do formulario, a função exibeMensagensDeErro, modificada o id da UL, modificado o nome da classe no CSS, e adicionado a variavel para limpar o formulario e os erros depois de adicionar um paciente válido

--- js/form.js
+++ js/form.js
@@ -11,10 +11,10 @@ botaoAdicionar.addEventListener("click", function(event) {
     var pacienteTr = montaTr(paciente);
 
     var erros = validaPaciente(paciente);
+    console.log(erros);
 
     if (erros.length > 0) {
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+        exibeMensagensDeErro(erros)
         return;
     }
 
@@ -33,6 +33,10 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     //reseta os dados dos campos para inserir o paciente no formulario
     form.reset();
+
+    //variavel para limpar o formulario e as mensagens de erro após incluir um paciente válido
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
     //função que pega os dados do paciente atravez de um objeto(var paciente)
     function obtemPacienteDoFormulario(form) {
@@ -80,14 +84,41 @@ botaoAdicionar.addEventListener("click", function(event) {
 
         var erros = [];
 
+        if(paciente.nome.length == 0){
+            erros.push("O nome não pode ser em branco");
+        }
+
+        if(paciente.peso.length == 0){
+            erros.push("O peso não pode ser em branco");
+        }
+
+        if(paciente.altura.length == 0){
+            erros.push("A altura não pode ser em branco");
+        }
+
+        if(paciente.gordura.length == 0){
+            erros.push("A gordura não pode ser em branco");
+        }
+
         if (!validaPeso(paciente.peso)) {
             erros.push("Peso é inválido!");
         }
 
         if (!validaAltura(paciente.altura)) {
-            erros.push("Altura é inválida");
+            erros.push("Altura é inválida!");
         }
 
         return erros;
+    }
+
+    //função que vai criar a ul, pegar a mensagem de erro, incluir o LI no UL e limpar os erros exibidos conforme for sendo preenchido de forma correta com o innerHTML
+    function exibeMensagensDeErro(erros){
+        var ul = document.querySelector("#mensagens-erro");
+        ul.innerHTML = "";
+        erros.forEach(function(erro){
+            var li = document.createElement("li");
+            li.textContent = erro;
+            ul.appendChild(li);
+        })
     }
 });
