184635274e9fe708bff93aae72b2390231f2f171
William Amorim <william.amorim@tedbrasil.com>
2017-12-11 18:04:09 -0200

    inserindo comentarios do arquivo remover-paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -4,10 +4,13 @@ var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event) {
 
+    //if para remover apenas se for um TD, e não um TH por exemplo(cabeçalho)
     if (event.target.tagName == 'TD') {
 
+        //adicionando a classe que remove no evento
         event.target.parentNode.classList.add("fadeOut");
 
+        //função para chamar a animação que remove
         setTimeout(function() {
             event.target.parentNode.remove();
         }, 500);
